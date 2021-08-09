f015eb1e3a8350f4e4be33a0ed0f1fdee7acbd23
noaki-k <knk43nok@gmail.com>
2021-08-10 08:13:27 +0900

    一般的方法としての手続き

--- 1-3-3.scm
+++ 1-3-3.scm
@@ -36,19 +36,24 @@
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
-  (define (close-enough? v1 v2)
-    (< (abs (- v1 v2)) tolerance))
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (close-enough? guess next)
-          next
-          (try next))))
-  (try first-guess))
+    (try f first-guess))
+
+(define (close-enough? v1 v2)
+  (< (abs (- v1 v2)) tolerance))
+
+(define (try f guess)
+  (let ((next (f guess)))
+     (if (close-enough? guess next)
+         next
+         (try f next))))
 
 (trace fixed-point) 
+(trace try) 
 (fixed-point cos 1.0)
-(fixed-point (lambda (y) (+ (sin y) (cos y))) 1)
+;(fixed-point (lambda (y) (+ (sin y) (cos y))) 1)
 
+(define (sqrt x)
+  (fixed-point (lambda (y) (avarage y (/ x y)) 1.0)))
 
 
 
