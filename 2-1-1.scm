165130ba1557b048e96bf6b33d97b4f214c4d7d3
noaki-k <knk43nok@gmail.com>
2021-08-17 08:39:38 +0900

    データ抽象

---
+++ 2-1-1.scm
@@ -0,0 +1,44 @@
+;(make-rat <n> <d>) 分子:n 分母:dの有理数を返す
+;(numer <x>) 有理数xの分子を返す
+;(denom <x>) 有理数xの分母を返す
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer x) (denom y))))
+
+(define x (cons 1 2))
+
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (add-rat one-third one-third))
