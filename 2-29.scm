a7dc0b49aee37dafac91afa8f4387cc8edc64189
noaki-k <knk43nok@gmail.com>
2021-08-20 12:06:44 +0900

    練習問題2.29

---
+++ 2-29.scm
@@ -0,0 +1,11 @@
+(define (make-mobil left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch branch)
+  ())
+(define (right-branch))
+(define (branch-length))
+(define (branch-structure))
